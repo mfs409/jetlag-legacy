1dcb770082bb37d1733c4da72619579be582a647
Michael Spear <spear@cse.lehigh.edu>
2018-10-26 23:53:09 -0400

    fixed routing... there was a rounding bug that was manifesting sometimes, leading the driver to immediately halt

--- src/jetlag/internal/support/RouteDriver.ts
+++ src/jetlag/internal/support/RouteDriver.ts
@@ -42,15 +42,17 @@ export class RouteDriver {
     private startRoute() {
         // move to the starting point
         let r = this.route.getPoint(0);
+        // convert start point from topleft to center, move actor to it
         this.actor.getBody().SetTransform(new XY(r.x + this.actor.getWidth() / 2, r.y + this.actor.getHeight() / 2), 0);
         // set up our next goal, start moving toward it
         this.nextIndex = 1;
         let p = this.route.getPoint(this.nextIndex)
+        // convert from the point to a unit vector, then set velocity
         p.x -= this.actor.getXPosition();
         p.y -= this.actor.getYPosition();
-        // normalize and scale the vector, then apply the velocity
         p.Normalize();
         p = p.Multiply(this.velocity);
+        console.log(p);
         this.actor.updateVelocity(p.x, p.y);
     }
 
@@ -61,38 +63,45 @@ export class RouteDriver {
             return;
         }
 
-        // if we haven't passed the goal, keep going. we tell if we've passed
-        // the goal by comparing the magnitudes of the vectors from source (s)
-        // to here and from goal (g) to here
+        // if we haven't passed the goal, keep going
         let source = this.route.getPoint(this.nextIndex - 1);
-        source.x -= this.actor.getXPosition();
-        source.y -= this.actor.getYPosition();
         let goal = this.route.getPoint(this.nextIndex)
-        goal.x -= this.actor.getXPosition();
-        goal.y -= this.actor.getYPosition();
-        let sameXSign = (goal.x >= 0 && source.x >= 0) || (goal.x <= 0 && source.x <= 0);
-        let sameYSign = (goal.y >= 0 && source.y >= 0) || (goal.y <= 0 && source.y <= 0);
+        let goalDx = source.x - goal.x;
+        let goalDy = source.y - goal.y;
+        let actorDx = source.x - this.actor.getXPosition();
+        let actorDy = source.y - this.actor.getYPosition();
+        // if goal left of source and actor right of goal, keep going
+        if ((goalDx > 0) && (goalDx > actorDx))
+            return;
+        // if goal right of source and actor left of goal, keep going
+        else if ((goalDx < 0) && (goalDx < actorDx))
+            return;
+        // if goal above source and actor below goal, keep going
+        if ((goalDy > 0) && (goalDy > actorDy))
+            return;
+        // if goal below source and actor above goal, keep going
+        else if ((goalDy < 0) && (goalDy < actorDy))
+            return;
 
-        if (((goal.x == goal.y) && (goal.x == 0)) || (sameXSign && sameYSign)) {
-            this.nextIndex++;
-            if (this.nextIndex == this.route.getNumPoints()) {
-                // reset if it's a loop, else terminate Route
-                if (this.loop) {
-                    this.startRoute();
-                } else {
-                    this.done = true;
-                    this.actor.updateVelocity(0, 0);
-                }
+        // Update the goal, and restart, stop, or start moving toward it
+        this.nextIndex++;
+        if (this.nextIndex == this.route.getNumPoints()) {
+            // reset if it's a loop, else terminate Route
+            if (this.loop) {
+                this.startRoute();
             } else {
-                // advance to next point
-                let p = this.route.getPoint(this.nextIndex)
-                p.x -= this.actor.getXPosition();
-                p.y -= this.actor.getYPosition();
-                p.Normalize();
-                p = p.Multiply(this.velocity);
-                this.actor.updateVelocity(p.x, p.y);
+                this.done = true;
+                this.actor.updateVelocity(0, 0);
             }
         }
-        // NB: 'else keep going at current velocity'
+        else {
+            // advance to next point
+            let p = this.route.getPoint(this.nextIndex)
+            p.x -= this.actor.getXPosition();
+            p.y -= this.actor.getYPosition();
+            p.Normalize();
+            p = p.Multiply(this.velocity);
+            this.actor.updateVelocity(p.x, p.y);
+        }
     }
 }
